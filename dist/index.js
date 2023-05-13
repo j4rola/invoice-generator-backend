3bf99fa8ba1314eb0aa542a8b232452042c6ebe2
Joe Froula <jmfroula@gmail.com>
2023-05-13 11:07:25 -0700

    switches pdf lib

--- dist/index.js
+++ dist/index.js
@@ -1,30 +1,77 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const pdfkit_1 = __importDefault(require("pdfkit"));
+const path_1 = __importDefault(require("path"));
 const express_1 = __importDefault(require("express"));
+const pdf = __importStar(require("html-pdf"));
+const fs_1 = __importDefault(require("fs"));
+const mustache_1 = __importDefault(require("mustache"));
 const app = (0, express_1.default)();
-app.get('/test', (req, res) => {
-    res.json({ test: 'test' });
-});
-const port = process.env.port || 3000;
+const templateFilePath = path_1.default.join(__dirname, '/index.html');
+const cssFilePath = path_1.default.join(__dirname, '/style.css'); // Path to your CSS file
 app.get('/download', (req, res) => {
-    const doc = new pdfkit_1.default();
-    doc.font('Helvetica');
-    // Add content to the PDF document   
-    doc.fontSize(25).fillColor('red').text('Hello, world!', { align: 'center' });
-    doc.fontSize(18).fillColor('red').text('Hello, world!', { align: 'center' });
-    const myName = 'my-invoice';
-    // Set the content type and attachment header     
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', `attachment; filename=${myName}.pdf`);
-    // Pipe the PDF document to the response    
-    doc.pipe(res);
-    // Close the PDF document and end the response  
-    doc.end();
+    // Read the HTML template file content
+    const template = fs_1.default.readFileSync(templateFilePath, 'utf-8');
+    // Read the CSS file content
+    const css = fs_1.default.readFileSync(cssFilePath, 'utf-8');
+    // Replace the placeholders in the template with actual values from the request
+    const html = mustache_1.default.render(template, {
+        name: 'test',
+        email: 'testing',
+        // Add more values as needed
+    });
+    // Combine HTML template and CSS styles
+    const htmlWithStyles = `
+    <html>
+      <head>
+        <style>
+          ${css}
+        </style>
+      </head>
+      <body>
+        ${html}
+      </body>
+    </html>
+  `;
+    // Generate the PDF from HTML
+    pdf.create(htmlWithStyles).toStream((err, stream) => {
+        if (err) {
+            res.status(500).send('An error occurred');
+            return;
+        }
+        // Set the appropriate headers for PDF response
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', 'attachment; filename=converted.pdf');
+        // Pipe the PDF stream to the response
+        stream.pipe(res);
+    });
 });
+const port = 3000;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,60 @@
-import PDFDocument from 'pdfkit'; 
-import fs from 'fs'; 
-import express from 'express'; 
-import { document } from 'pdfkit/js/page';
+import path from 'path';
+import express from 'express';
+import * as pdf from 'html-pdf';
+import fs from 'fs';
+import mustache from 'mustache';
 
-const app = express(); 
+const app = express();
+const templateFilePath = path.join(__dirname, '/index.html');
+const cssFilePath = path.join(__dirname, '/style.css'); // Path to your CSS file
 
-app.get('/test', (req, res) => {          
-  res.json({test: 'test'})
+app.get('/download', (req, res) => {
+  // Read the HTML template file content
+  const template = fs.readFileSync(templateFilePath, 'utf-8');
+
+  // Read the CSS file content
+  const css = fs.readFileSync(cssFilePath, 'utf-8'); 
+
+  // Replace the placeholders in the template with actual values from the request
+  const html = mustache.render(template, {
+    name: 'test',
+    email: 'testing',    
+    // Add more values as needed
+  });
+
+  // Combine HTML template and CSS styles
+  const htmlWithStyles = `
+    <html>
+      <head>
+        <style>
+          ${css}
+        </style>
+      </head>
+      <body>
+        ${html}
+      </body>
+    </html>
+  `;
+
+  // Generate the PDF from HTML
+  pdf.create(htmlWithStyles).toStream((err, stream) => {
+    if (err) {
+      res.status(500).send('An error occurred');
+      return;
+    }
+
+    // Set the appropriate headers for PDF response
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'attachment; filename=converted.pdf');
+
+    // Pipe the PDF stream to the response
+    stream.pipe(res);
+  });
 });
 
-const port = process.env.port || 3000
-
- 
-app.get('/download', (req, res) => {          
-  const doc = new PDFDocument();       
-  
-  doc.font('Helvetica')
-  // Add content to the PDF document   
-  doc.fontSize(25).fillColor('red').text('Hello, world!',  {align: 'center'}); 
-  doc.fontSize(18).fillColor('red').text('Hello, world!',  {align: 'center'});   
-  
-  const myName = 'my-invoice'      
-  
-  // Set the content type and attachment header     
-  res.setHeader('Content-Type', 'application/pdf');       
-  res.setHeader('Content-Disposition', `attachment; filename=${myName}.pdf`);    
-  
-  // Pipe the PDF document to the response    
-  doc.pipe(res);   
-  
-  // Close the PDF document and end the response  
-  doc.end();  
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
 
-app.listen(port, () => {                          
-  console.log(`Server started on port ${port}`);     
-});   
+
