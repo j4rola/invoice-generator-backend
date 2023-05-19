0b52cc9b0a191f76322442b99a75bcf2139b411f
Joe Froula <jmfroula@gmail.com>
2023-05-19 11:01:26 -0700

    test

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ app.get('/generate-pdf', async (req, res) => {
     
     // Navigate to the page that the client is on
     //await page.goto(req.headers.referer || '', { waitUntil: 'networkidle0' });
-    await page.goto('https://invoice-generator-frontend-5p3c.vercel.app/invoice');
+    await page.goto('https://invoice-generator-frontend-5p3c.vercel.app/');
     
     // Generate the PDF stream
     const pdfStream = await page.pdf({ 
